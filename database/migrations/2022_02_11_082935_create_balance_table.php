8f5b9f7894a15d52a5c6f4da7598b3aea49ef120
Murindangabo cyuzuzo p.damien <zudanga@gmail.com>
2022-02-11 10:46:17 +0200

    after finishup with migrations

---
+++ database/migrations/2022_02_11_082935_create_balance_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBalanceTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('balance', function (Blueprint $table) {
+            $table->id();
+            $table->decimal('amount', 10, 2);
+            $table->integer('user_id');
+            $table->string('mode');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('balance');
+    }
+}
